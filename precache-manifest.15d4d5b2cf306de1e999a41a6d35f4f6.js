2066e075fcc344967eb6ffd9a7cd51f7246ec3a8
tusharravindran <tusharrravindran@gmail.com>
2022-04-03 16:51:35 +0530

    Updates

---
+++ precache-manifest.15d4d5b2cf306de1e999a41a6d35f4f6.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3681005a1c1dfc11b547aa58a8a9b532",
+    "url": "/index.html"
+  },
+  {
+    "revision": "826fec28bb9fea1ee1db",
+    "url": "/static/css/main.022eb82c.chunk.css"
+  },
+  {
+    "revision": "cd3fbc320edd4fb893b1",
+    "url": "/static/js/2.372055bd.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/static/js/2.372055bd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "826fec28bb9fea1ee1db",
+    "url": "/static/js/main.1bfb847f.chunk.js"
+  },
+  {
+    "revision": "f52ba179432c3be28857",
+    "url": "/static/js/runtime-main.554504a0.js"
+  },
+  {
+    "revision": "9dc95b9e0da0f6c0c27e88698a2afdb3",
+    "url": "/static/media/Django.9dc95b9e.jpeg"
+  },
+  {
+    "revision": "a8bfac06c6cce76b9e205e304091a055",
+    "url": "/static/media/TUSHAR R_PARTICIPATION.a8bfac06.jpg"
+  },
+  {
+    "revision": "d844fb2385009c4eb11f195c3a39fc22",
+    "url": "/static/media/cisconet.d844fb23.png"
+  },
+  {
+    "revision": "82590484d12ea365a2b84cbb916c50ce",
+    "url": "/static/media/resum_hack.82590484.png"
+  },
+  {
+    "revision": "7163560802e4e6fcec2e4b080f9bbe1f",
+    "url": "/static/media/resume.71635608.pdf"
+  },
+  {
+    "revision": "d55deb0ceb75c88c7b8afcadad6bd80c",
+    "url": "/static/media/tusharravindran.d55deb0c.png"
+  }
+]);
